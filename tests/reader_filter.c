4074c9a993574ee53626ef86017af4ad82eb1c81
Data7Viz <data7vizz@gmail.com>
2026-06-27 12:02:31 +0200

    Добавил условие в sscanf колличество прочитаных и потеряных сообщений

--- tests/reader_filter.c
+++ tests/reader_filter.c
@@ -19,29 +19,38 @@ int main (int argc, char *argv [])
 	double time = 0.0;
 	uint32_t id = 0;
 	uint16_t data [8] = {0};
-	uint32_t buf_analize [65535] = {0};
-	uint32_t kol_string = 0;
+	uint32_t buf_analize [65536] = {0};
+	uint32_t all_string = 0;
 	uint32_t poisk = 0;
+	uint32_t read_string = 0;
+	uint32_t kol_poter_string = 0;
 	while (fgets (buf_file, sizeof (buf_file), file))
 	{
-		kol_string ++;
-		sscanf (buf_file, " (%lf) %*s %x %*[hhd] %hx %hx %hx %hx %hx %hx %hx %hx", &time,&id,&data[0],&data[1],&data[2],&data[3],&data[4],&data[5],&data[6],&data[7]);
-        		        
-		poisk = id & 0xff;
-		buf_analize [poisk] ++; 
+		all_string ++;
+		if (sscanf (buf_file, " (%lf) %*s %x [%*d] %hx %hx %hx %hx %hx %hx %hx %hx", &time,&id,&data[0],&data[1],&data[2],&data[3],&data[4],&data[5],&data[6],&data[7]) == 10);
+		{
+         	poisk = id & 0xff;
+		buf_analize [poisk] ++;
+	        read_string ++;	
+		} 
 	} 
 	fclose (file);
 	
 	printf ("\n");
-	for (int i = 0; i < 65535; i ++)
+	for (int i = 0; i <= 65535; i ++)
 	{
+		
 		if (buf_analize [i] > 0)
 		{
-			printf (" номер блока %d   \t  отправленно сообщений %u\n", i, buf_analize [i]);
+			printf (" номер блока %X   \t  отправленно сообщений %u\n", i, buf_analize [i]);
+
 		}
 	}
 	printf ("\n");
-	printf (" Всего сообщений %u\n\n", kol_string);	
+	kol_poter_string = all_string - read_string;
+	printf (" Всего сообщений \t %u\n", all_string);
+	printf (" Прочитаные сообщения \t %u\n", read_string);	
+	printf (" Пропущенно  сообщений \t %u\n", kol_poter_string);
 	return 0;
 } 	
 
