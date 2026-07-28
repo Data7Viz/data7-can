f93348e9241688249e2edf38c8681663ca5226ef
Data7Viz <data7vizz@gmail.com>
2026-07-28 19:05:03 +0300

    актуальный код fibonacci.c

--- fib-fac-eucl/fibonacci.c
+++ fib-fac-eucl/fibonacci.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#define GRIN "\033[0;32m"
+#define RES "\033[0m"
 int main ()
 {
-	unsigned int a = 0, b = 1, res = 0, fib_num = 0;
+	unsigned int a = 0, b = 1, res = 0, fib_num = 0, bit = 0, count = 28, id = 0x0CF00400;
 	printf ("Введи число Фибонначчи : ");
 	scanf ("%u", &fib_num);
 	printf ("\n");
@@ -10,7 +12,13 @@ int main ()
 		res = a + b;
 		b = a; 
 		a = res;
-		printf ("%-3u число Фибоначчи = %u\n", i, res);
+		bit = (id >> count) & 1;	
+		printf ("\t %-2u bit %u "GRIN" | "RES" ряд %-3u число = %u\n", count, bit, i, res);
+		count --;
+		if (i == 3 || i == 5 || i == 13 || i == 21)
+		{ 
+			printf ("\n");
+		} 
 	} 
 	return 0;
 } 
