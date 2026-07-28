dd5e73508d7daecc8c5dc40714c2022ba2163def
Data7Viz <data7vizz@gmail.com>
2026-07-28 13:40:48 +0300

    добавил ряды F-3   2

--- tests/fibonacci.c
+++ tests/fibonacci.c
@@ -3,12 +3,13 @@ int main ()
 {
 	unsigned int a = 0, b = 1, res = 0, fib_num = 0;
 	scanf ("%u", &fib_num);
+	printf ("\n");
 	for (int i = 1; i <= fib_num; i ++)
 	{
 		res = a + b;
 		b = a; 
 		a = res;
-		printf ("%u\n", res);
+		printf (" F-%-3u %u\n", i, res);
 	} 
 	return 0;
 } 
