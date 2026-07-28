d8d69031ccb1b6f1bef9881ccafcd470ef1c2995
Data7Viz <data7vizz@gmail.com>
2026-07-28 13:57:58 +0300

    добавил функции printf, scanf в factorial.c

--- tests/factorial.c
+++ tests/factorial.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main ()
 {
-	unsigned int a = 0, res = 1;
-	for (int i = 1; i <= 8; i ++)
+	unsigned int fact_num = 0, res = 1;
+	printf ("Введи число, чтобы вычислить его факториал : ");
+	scanf ("%u", &fact_num); 
+	for (int i = 1; i <= fact_num; i ++)
 	{
 		res *= i;
 		printf (" %u! = %u\n", i, res);
