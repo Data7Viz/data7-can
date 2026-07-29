1ca1aa81920bbcf00db508047e6ed3664e214104
Data7Viz <data7vizz@gmail.com>
2026-07-30 00:05:28 +0300

    наложил структуру ID J1939 по новому

--- fib-fac-eucl/fibonacci.c
+++ fib-fac-eucl/fibonacci.c
@@ -3,30 +3,31 @@
 #define RES "\033[0m"
 void print ()
 {
-	printf (GRIN"\t     bit | ряд    число  |  пара     |  del    | del \n"RES);
+	printf (GRIN"\t     bit | ряд    число  |  хэш fib \n"RES);
 } 
 int main ()
 {
-	double a = 0, b = 1, res = 0, del = 0, to_fib = 0, del1 = 0; 
-	unsigned int count = 28, id = 0x0CF00400, bit = 0;
+	unsigned int a = 0, b = 1, res = 0, fib = 0, byf_fib = 0; 
+	unsigned int count = 28, id = 0, bit = 0;
+	scanf (" %i", &id);
 	print (); 
 	for (int i = 1; i <= 29; i ++)
 	{
 		res = a + b;
-		del = b / a; 
-		del1 = a / b;
 		b = a; 
 		a = res;
-		to_fib = res * 2;
-		bit = (id >> count) & 1;	
-		printf ("\t %-2u   %u "GRIN" | "RES" %-3u   %-6.0lf"GRIN" | "RES"  %-7.0lf "GRIN"|"RES" %-7.4lf "GRIN"|"RES" %-4.4lf\n", count, bit, i, res, to_fib, del, del1);
+		bit = (id >> count) & 1;
+		fib = bit * res;
+		byf_fib += fib;
+		printf ("\t %6u "GRIN" | "RES" %-3u   %-6.0u"GRIN" | "RES" %-5u\n", bit, i, res, fib);
 		count --;
 		if (i == 3 || i == 5 || i == 13 || i == 21)
 		{ 
 			printf ("\n");
 		} 
 	} 
-	printf (GRIN"\t\t\t\t%X\n"RES, id); 
+	printf (GRIN"ID J1939 0x%X  %20u\n"RES, id, byf_fib); 
+	 
 	return 0;
 } 
 
