67f43a68dc43a0f0ea64dbe394317ad12f5b28ec
Data7Viz <data7vizz@gmail.com>
2026-07-29 12:39:31 +0300

    писал код в termux

--- fib-fac-eucl/fibonacci.c
+++ fib-fac-eucl/fibonacci.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #define GRIN "\033[0;32m"
 #define RES "\033[0m"
+void print ()
+{
+	printf (GRIN"\t     bit | ряд    число  |  пара     |  del    | del \n"RES);
+} 
 int main ()
 {
-	unsigned int a = 0, b = 1, res = 0, bit = 0, count = 28, id = 0x0CF00400, to_fib = 0;
+	double a = 0, b = 1, res = 0, del = 0, to_fib = 0, del1 = 0; 
+	unsigned int count = 28, id = 0x0CF00400, bit = 0;
+	print (); 
 	for (int i = 1; i <= 29; i ++)
 	{
 		res = a + b;
+		del = b / a; 
+		del1 = a / b;
 		b = a; 
 		a = res;
 		to_fib = res * 2;
 		bit = (id >> count) & 1;	
-		printf ("\t № %-2u bit %u "GRIN" | "RES" ряд - %-3u число = %-6u"GRIN" | "RES"пар - %-7u\n", count, bit, i, res, to_fib);
+		printf ("\t %-2u   %u "GRIN" | "RES" %-3u   %-6.0lf"GRIN" | "RES"  %-7.0lf "GRIN"|"RES" %-7.4lf "GRIN"|"RES" %-4.4lf\n", count, bit, i, res, to_fib, del, del1);
 		count --;
 		if (i == 3 || i == 5 || i == 13 || i == 21)
 		{ 
