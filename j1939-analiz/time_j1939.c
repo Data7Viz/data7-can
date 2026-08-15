8fe9d8e66bd3820ff3d3938666056d45223bc211
Data7Viz <data7vizz@gmail.com>
2026-08-15 17:05:00 +0300

    убрал сырое время добавил номера строк

--- j1939-analiz/time_j1939.c
+++ j1939-analiz/time_j1939.c
@@ -7,51 +7,6 @@
 #define GR "\033[0;90m"
 #define RES "\033[0m"
 
-void print (char *cvet, char *stroka)
-{
-	printf ("%s%s---------------------------------------------------------------------------------------------------\n\n"RES,cvet,stroka);
-} 
-// функция для анализа 0,1,2,3 байта 
-void fyn_for_byt1_byt2_byt3 (uint32_t *arr, char *str)
-{
-	int a = 0;
-	for (int i = 0; i <= 255; i ++)
-	{
-		if (arr [i] > 0) { printf (""GR" |"RES" %s"SIN" %-2X"RES" "GRIN"%-3u"RES" сбщ %-7u", str, i, i, arr [i]);
-			a ++; if (a % 6 == 0) printf ("\n"); } 
-	}
-} 
-
-// функция адресные вещательные сообщения 
-void fyn_pgn (uint32_t *arr_prior, uint32_t *arr, uint32_t min, uint32_t max)
-{
-	int b = 0;
-	for (int i = min; i <= max; i ++) 
-	{ 
-		if (arr [i] > 0)
-		{	
-				printf (""GR" |"RES" %u"SIN" %4X"RES" "GRIN"%-5u"RES" сбщ %-7u ", arr_prior [i], i, i, arr [i]); 
-				b ++; if (b % 6 == 0) printf ("\n"); 
-		} 
-	}
-} 
-
-// функция сообщения с блока на блок
-void fyn_su_dt (uint32_t su_dt [256] [256])
-{
-	int c = 0;
-	for (int i = 0; i <= 255; i ++)
-	{
-		for (int j = 0; j <= 255; j ++)
-		{
-			if (su_dt [i] [j] > 0)
-			{
-				printf (""GR" |"RES" ис"SIN" %-2X"RES" наз"SIN" %-2X "RES" сбщ %-7u", i, j, su_dt [i] [j]); 
-				c ++; if (c % 6 == 0) printf ("\n");
-			}
-		}
-	}
-} 
 int main (int argc, char *argv [])
 {
 	if (argc < 2) { printf (" Ошибка -> Укажи имя файла \n"); return 1; }
@@ -67,11 +22,10 @@ int main (int argc, char *argv [])
 	uint32_t all_frame = 0; // все фреймы 
 	uint32_t read_frame = 0; // прочитаные фрейьы
 	uint32_t kol_poter_frame = 0; // потеряные фреймы 
-	// память для логики 
-	uint32_t arr_byte1 [256] = {0}, arr_byte2 [256] = {0}, arr_byte3 [256] = {0}, arr_br_byte3 [256] = {0}, arr_pgn [65536] = {0}, arr_prior [65536] = {0}, arr_sour_dist [256] [256] = {0}; 
+	
+	// память для логики  
 	uint32_t byte0 = 0, byte1 = 0, byte2 = 0, byte3 = 0, pgn = 0, prior = 0;	
-	// счётчики вещательных и адресных сообщений общее колличество
-	uint32_t count_br = 0, count_addr = 0;
+
 	// длительность снятия лога 
 	double start_time = 0.0, end_time = 0.0;
 	uint32_t prochl_id = 0;	
@@ -95,28 +49,26 @@ int main (int argc, char *argv [])
 		if (read_frame == 1) 
 		{ 
 			start_time = time;
-			frame_0 = ceil (start_time); // обнуляю разряды после точки стартового сообщения
-			frame_jmp = frame_0 + 1; // прыгаю на 1 секунду вперё
+			// frame_0 = ceil (start_time); обнуляю разряды после точки стартового сообщения
+			frame_0 = time;
+			frame_jmp = frame_0 + 0.2; // прыгаю на 1 секунду вперё
 		}   	
-	 
+	
 		if ((time >= frame_0) && (time <= frame_jmp)) 
 		{
-				if (byte1 >= 240)
-				{	
+			
 				if (prochl_time  > 0.0)
-				{
+				{ 
 				zazor = time - prochl_time; // вычисляем зазор между кадрами 
-				printf ("    "GRIN"%-18lf %-8X"RES" зазор %-10lf"SIN"%-8X %-18lf"RES, prochl_time, prochl_id, zazor, id, time); 
+				printf (" %-5u "GRIN" %-8X"RES" зазор %-10lf"SIN"%-8X "RES, count_0_jmp, prochl_id, zazor, id); 
 				
 				count_0_jmp ++; // счётчик сообщений 
 				
-				if (count_0_jmp % 2 == 0) printf ("\n"); // отрисовка таблицы  
+				if (count_0_jmp % 4 == 0) printf ("\n"); // отрисовка таблицы  
 				}
 					prochl_time = time; // берём прошлое время 
 					prochl_id = id;
-				}       
-	  		
-	
+	   
 		} 
 		
 		} 
@@ -124,45 +76,13 @@ int main (int argc, char *argv [])
 		     } 
 	} 
 	fclose (file);
-	//printf ("\t СООБЩЕНИЙ %u  ЗАГРУЗКА СЕТИ %lf\n", count_0_jmp, count_0_jmp * 0.000500);
-	int p = 0;
-	//for (int i = 0; i <= 65535; i ++)
-	//{
-		//if (max_zazor [i] > 0.0)
-		//{
-			//printf (" | %-5X  %-10lf", i, max_zazor [i]);
-			//p ++; if (p % 8 == 0) printf ("\n");
-			
-		//}
-	//} 
-	printf ("\n");	
-	//printf ("\n");
-	//print (GOT, "[ Байт 1 ] Сетевые / Транспортные ---------------------------------"); fyn_for_byt1_byt2_byt3 (arr_byte1, "byte 1");
-	//printf ("\n\n");
-
-	//print (GOT, "[ Байт 2 ] Блоки источники вещательных сообщений [ байт 1 >= 240 ] "); fyn_for_byt1_byt2_byt3 (arr_br_byte3, "бл ист");
-        //printf	 ("\n\n");
-
-	//print (GOT, "[ Байт 3 ] Все блоки источники ------------------------------------"); fyn_for_byt1_byt2_byt3 (arr_byte3, "бл ист");
-        //printf ("\n\n"); 	
-
-	//print (GOT, "[ Байт 2 Байт 3 ] Сообщение с блока на блок [ byte 1 < 240 ] ------"); fyn_su_dt (arr_sour_dist);
-	//printf ("\n\n");
-
-	//print (GOT, "[ байт 2 Байт 3 ] Адресные сообщения [ byte 1 < 240 ] -------------"); fyn_pgn (arr_prior, arr_pgn, 0, 61439);
-	//printf ("\n\n");
-	
-	//print (GOT, "[ Байт 1 Байт 2] Вещательные сообщения [ byte 1 >= 240 ] ----------");  fyn_pgn (arr_prior, arr_pgn, 61440, 65279);
-	//printf ("\n\n");
-
-	//print (GOT, "[ Байт 1 Байт 2 ] Заводские Проприетарные [ byte 1 >= 240 ] -------"); fyn_pgn (arr_prior, arr_pgn, 65280, 65535);
-	//printf ("\n\n");
+	double zagr_seti = ((count_0_jmp * 0.000500) / (frame_jmp - frame_0)) * 100.0;
+	printf ("\t СООБЩЕНИЙ %u  ЗАГРУЗКА СЕТИ %lf\n", count_0_jmp, zagr_seti);
 	 
 	
 	// КОЛЛИЧЕСТВО прочитаных фреймов
-	//kol_poter_frame = all_frame - read_frame;
-	//printf (GRIN"Всего %-7u Прочитано %-7u Пропущено %-7u"RES" "GOT" Длительность лога %-7lf сек"RES, all_frame, read_frame, kol_poter_frame, end_time - start_time);
-	//printf (SIN"\tВещательных %-7u  Адресных %-7u\n"RES, count_br, count_addr);
+	kol_poter_frame = all_frame - read_frame;
+	printf (GRIN"Всего %-7u Прочитано %-7u Пропущено %-7u"RES" "GOT" Длительность лога %-7lf\n сек"RES, all_frame, read_frame, kol_poter_frame, end_time - start_time);
 	return 0;
 } 	
 
