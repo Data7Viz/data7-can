d532808457efc38c38dacca48b428b608231511d
Data7Viz <data7vizz@gmail.com>
2026-08-28 15:28:50 +0300

    разработка утилиты time_j1939_manual.c ещё не переименновал

--- j1939-analiz/time_j1939.c
+++ j1939-analiz/time_j1939.c
@@ -7,6 +7,12 @@
 #define GR "\033[0;90m"
 #define RES "\033[0m"
 
+void tichina (double time, double prochl_time, uint32_t id, uint32_t prochl_id)
+{
+	double zazor = (time - prochl_time) - 0.000500;
+	printf (" %X %lf  %X\n", prochl_id, zazor, id);
+} 
+
 int main (int argc, char *argv [])
 {
 	if (argc < 2) { printf (" Ошибка -> Укажи имя файла \n"); return 1; }
@@ -18,74 +24,79 @@ int main (int argc, char *argv [])
 	double time = 0.0;
 	uint32_t id = 0;
 	uint16_t data [8] = {0};
-	uint16_t a = 0;
-	uint32_t all_frame = 0; // все фреймы 
-	uint32_t read_frame = 0; // прочитаные фрейьы
-	uint32_t kol_poter_frame = 0; // потеряные фреймы 
 	
 	// память для логики  
 	uint32_t byte0 = 0, byte1 = 0, byte2 = 0, byte3 = 0, pgn = 0, prior = 0;	
 
 	// длительность снятия лога 
 	double start_time = 0.0, end_time = 0.0;
-	uint32_t prochl_id = 0;	
+	uint32_t prochl_id = 0, read_frame = 0;	
 	
 	// переменные для анализа времени
 	double frame_start = 0.0, frame_jmp = 0.0;
-	double prochl_time = 0.0, time_tichina = 0.0, zazor = 0.0; 
-	uint32_t count_0_jmp = 0;
+	double prochl_time = 0.0, zazor = 0.0; 
+	uint32_t count_addr = 0, count_broad = 0;
+	uint16_t prochl_data [8] = {0}; 
+
 
 	while (fgets (byf_file, sizeof (byf_file), file))
 	{
-		all_frame ++; // все фреймы
 		// парсим очищвем логи
 		for (int i = 0; i <= 7; i ++) data [i] = 0;
-		if (sscanf (byf_file," (%lf) %*s %x [%*d] %hx %hx %hx %hx %hx %hx %hx %hx", &time,&id,&data[0],&data[1],&data[2],&data[3],&data[4],&data[5],&data[6],&data[7]) == 10)
-		{
-		end_time = time; // переменная для вычисления длительности лога конечное время 
+		sscanf (byf_file," (%lf) %*s %x [%*d] %hx %hx %hx %hx %hx %hx %hx %hx", 
+				&time,&id,&data[0],&data[1],&data[2],&data[3],&data[4],&data[5],&data[6],&data[7]);
+		
 		read_frame ++;
-		byte0 = (id >> 24) & 0xff; byte1 = (id >> 16) & 0xff; byte2 = (id >> 8) & 0xff; byte3 = id & 0xff, pgn = (id >> 8) & 0xffff, prior = (id >> 26) & 7;  
+		end_time = time; // переменная для вычисления длительности лога конечное время 
+		byte0 = (id >> 24) & 0xff; byte1 = (id >> 16) & 0xff; byte2 = (id >> 8) & 0xff; 
+		byte3 = id & 0xff, pgn = (id >> 8) & 0xffff, prior = (id >> 26) & 7;  
 		
 		if (read_frame == 1) 
 		{ 
 			start_time = time; // переменная для вычисления длительности лога стартовое время 
 			// frame_0 = ceil (start_time); обнуляю разряды после точки стартового сообщения
-			frame_start = time;
-			frame_jmp = frame_start + 1.0; // прыгаю на 1 секунду вперёд
+			frame_start = time + 2;
+			frame_jmp = frame_start + 0.1; // прыгаю на 1 секунду вперёд
 		}   	
 	
 		if ((time >= frame_start) && (time <= frame_jmp)) 
 		{
-			
-			if (pgn == 0xF004)
-			{	
-			if (prochl_time  > 0.0)
-				{ 
-				zazor = (time - prochl_time) - 0.000500; // вычисляем зазор между кадрами минус длительность кадра 500 мкс
-				time_tichina += zazor; // общее время тишины в сети 
-				count_0_jmp ++; // счётчик сообщений 
-				printf ("\tсбщ %-5u ->"GRIN" %-8X"RES" тишина сети %-10lf"SIN"%-8X"RES, count_0_jmp, prochl_id, zazor, id); 
-				
-				if (count_0_jmp % 2 == 0) printf ("\n"); // отрисовка таблицы  
+			short count_n = 0;
+			count_n ++;		
+			if (byte1 < 240) { count_addr ++; } 
+			else { count_broad ++; }  
+
+			if (prochl_time > 0.0)
+			{
+				zazor = (time - prochl_time) - 0.000500;
+				printf (SIN"\t\t ID %08X DATA "RES, prochl_id);
+			      	for (short i = 0; i <= 7; i ++)
+				{
+					printf (SIN" %02hX"RES, prochl_data [i]);
 				}
-					prochl_time = time; // берём прошлое время 
-					prochl_id = id;
-	   		} 
-		} 
+				printf ("\n");	
+				printf (" ТИШИНА СЕТИ %lf\n "GRIN"\t\t ID %08X DATA "RES,zazor, id);
+				for (short a = 0; a <= 7; a ++)
+				{
+					printf (GRIN" %02hX"RES, data [a]);
+				}
+			       	printf ("\n\n");	
+			}
+			prochl_time = time;
+			prochl_id = id;
+			for (short j = 0; j <= 7; j ++)
+			{
+			prochl_data [j] = data [j];
+			} 
 		
-		} 
-		else { //printf ("Не прочитаные : %s", byf_file); 
-		     } 
-	} 
+		} // if ((time >= frame_start)...) 
+		 
+	} // while 
 	fclose (file);
-	double zagr_seti = ((count_0_jmp * 0.000500) / (frame_jmp - frame_start)) * 100.0;
-	printf ("\n "SIN"Анализируем"RES" %lf сек "SIN"Сообщений "RES"%u  "SIN"Загрузка сети "RES"%lf "SIN"Общее время тишины сети"RES" %lf\n ", 
-			frame_jmp - frame_start, count_0_jmp, zagr_seti, time_tichina);
-	 
 	
-	// КОЛЛИЧЕСТВО прочитаных фреймов
-	kol_poter_frame = all_frame - read_frame;
-	printf (GRIN"Всего сообщений %-7u Прочитано %-7u Пропущено %-7u"RES" "GOT" Длительность лога %-7lf сек \n"RES, all_frame, read_frame, kol_poter_frame, end_time - start_time);
+	printf ("Анализируем %lf сек Адресных сообщений %u Вещательных сообщений %u Всего %u\n", 
+			frame_jmp - frame_start, count_addr, count_broad, count_addr + count_broad); 
+	 
 	return 0;
 } 	
 
